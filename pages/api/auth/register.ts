f7a11936d9b6de2f2cbe6c76c9215e9a14265182
JessseG <61717513+JessseG@users.noreply.github.com>
2022-03-01 20:55:57 -0500

    Recovered Files - Secured Mailer - Activation ( pUser ->  vUser )

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -4,9 +4,10 @@ import { getSession } from "next-auth/react";
 import { hash } from "bcrypt";
 import sendConfirmationEmail from "./mailer";
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import moment from "moment";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { user } = req.body;
+  const { pUser } = req.body;
   const session = await getSession({ req });
   // const express = require("express");
   // const cors = require("cors");
@@ -22,6 +23,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   //   data: ,
   // }
 
+  const verifyInt = (str) => {
+    for (let i = 0; i < str.length; i++) {
+      if (!Number.isInteger(parseInt(str[i]))) {
+        return false;
+      }
+    }
+    return true;
+  };
+
   if (session) {
     return res.status(500).json({ error: "You are already logged in" });
   }
@@ -29,12 +39,38 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     // Store hash in password db
     // Check if a user with email already exists
+    // First Validate all the entries
+    const regexp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (
+      !regexp.test(pUser.email) ||
+      !pUser.name ||
+      /^\s*$/.test(pUser.name) ||
+      !pUser.email ||
+      /^\s*$/.test(pUser.email) ||
+      !pUser.dobDay ||
+      !pUser.dobYear ||
+      pUser.dobMonth === null ||
+      !verifyInt(pUser.dobDay) ||
+      !verifyInt(pUser.dobYear) ||
+      pUser.dobMonth === null ||
+      !moment.months().includes(pUser.dobMonth) ||
+      0 === parseInt(pUser.dobDay) ||
+      31 < parseInt(pUser.dobDay) ||
+      parseInt(pUser.dobYear) < 1900 ||
+      new Date().getFullYear() < parseInt(pUser.dobYear) ||
+      pUser.password.length < 8
+    ) {
+      return res.status(500).json({ error: "Invalid entry" });
+    }
+
     try {
       const pendingUser = await prisma.p_User.findUnique({
-        where: { email: String(user.email) },
+        where: { email: String(pUser.email) },
       });
       const existingUser = await prisma.user.findUnique({
-        where: { email: String(user.email) },
+        where: { email: String(pUser.email) },
       });
       if (pendingUser || existingUser) {
         return res.json({ status: "failure", error: "Email is taken" });
@@ -43,15 +79,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(500).json({ error: e });
     }
 
-    hash(user.password, 10, async function (err, hash) {
+    hash(pUser.password, 10, async function (err, hash) {
       try {
-        const pUser = await prisma.p_User.create({
+        const pendUser = await prisma.p_User.create({
           data: {
-            name: user.name,
-            email: user.email,
-            dobDay: user.dobDay,
-            dobMonth: user.dobMonth,
-            dobYear: user.dobYear,
+            name: pUser.name,
+            email: pUser.email,
+            dobDay: pUser.dobDay,
+            dobMonth: pUser.dobMonth,
+            dobYear: pUser.dobYear,
             password: hash,
           },
         });
@@ -63,7 +99,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             headers: {
               "Content-Type": "application/json",
             },
-            data: { email: pUser.email, name: pUser.name, hash: pUser.id },
+            data: {
+              email: pendUser.email,
+              name: pendUser.name,
+              hash: pendUser.id,
+            },
           };
 
           try {
